f9ffd546fef7bdadf133c22395f82c8acaff80f8
mohamed kamal <kmido9729@gmail.com>
2024-04-11 21:38:40 +0200

    api is fun

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
