f09ddd0827ea32ee2a53fa8830065789a5aeec8a
mohamed kamal <kmido9729@gmail.com>
2024-04-11 21:44:13 +0200

    api is fun

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles"""
+"""
+function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyBot/0.0.1'}
-    response = requests.get(url, headers=headers)
+    """titles of the first 10 hot posts"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json()
+    hot_post = data['data']['children']
+
+    for post in hot_post[:10]:
+        print("{}".format(post['data']['title']))
