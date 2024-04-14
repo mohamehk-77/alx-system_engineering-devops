3315a283adb5f7a64faf026a2a2087b7041f40c8
mohamed kamal <kmido9729@gmail.com>
2024-04-14 22:43:29 +0200

    momo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,10 @@
+# Define a resource to fix the typo in wp-settings.php
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin:/bin/',
+  # Run the command only if the file contains "phpp"
+  onlyif  => '/bin/grep -q "phpp" /var/www/html/wp-settings.php',
 }
+
+# Include the class with the fix resource in your main manifest
+class { 'main': }
