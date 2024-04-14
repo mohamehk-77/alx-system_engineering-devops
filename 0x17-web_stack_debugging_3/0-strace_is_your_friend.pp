d0d5447833f55f28fcb3431f8fbff09eb339fbce
mohamed kamal <kmido9729@gmail.com>
2024-04-14 22:49:49 +0200

    momo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# Fix a missing file causing the 500 error
-exec { 'fix-wordpress':
-  command => 'touch /var/www/html/wp-settings.php',  # Create the missing file
-  path    => '/usr/local/bin:/bin/',
-  onlyif  => 'test ! -f /var/www/html/wp-settings.php',  # Only run if file is missing
-}
+# fix apachi server to run properly run wordpress site 
 
-# Ensure Apache user has read access to wp-settings.php
-file { '/var/www/html/wp-settings.php':
-  ensure => present,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0644',
+exec { 'fix_wp_settings':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/local/bin/', '/bin/'],
 }
